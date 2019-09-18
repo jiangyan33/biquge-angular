9283a9c451d7e221e4006f286c58d553a87b4267
石江山 <shijiangshan@bjknrt.com>
2019-09-18 18:21:26 +0800

    新增express

---
+++ dist/biquge-angular/assets/index.js
@@ -0,0 +1,8 @@
+/**
+ * 静态文件服务器
+ */
+const path = require('path');
+const express = require("express");
+const app = express();
+app.use("/", express.static(path.join(__dirname, "../")));
+app.listen(80);
