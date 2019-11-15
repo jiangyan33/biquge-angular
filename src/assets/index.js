a304caf2bd2a56da3ccf188fb8cfeca527138376
石江山 <shijiangshan@bjknrt.com>
2019-11-15 16:31:06 +0800

    删除不需要的文件

--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 静态文件服务器
- */
-const path = require('path');
-const express = require("express");
-const app = express();
-app.use("/", express.static(path.join(__dirname, "../")));
-app.listen(80);
